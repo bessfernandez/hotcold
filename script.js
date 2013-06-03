6bdcb512a8ad3f4e7dfda9bd88ead753be5d764a
Bess <bessfernandez@gmail.com>
2013-06-02 23:38:12 -0400

    further work on nativ hot/cold

--- script.js
+++ script.js
@@ -3,40 +3,38 @@ window.onload = function() {
      rg = 25,
      s = document.getElementById('status'),
      txt = s.innerHTML,
-     arr = [],
      st = ["hotter", "colder", "correct"],
      cl = ["red", "blue", "green"],
-     pr = null;
- 
+     pr = null,
+     prdiff = null;
  document.getElementById('submit').onclick = function(event) {
  	 var v = document.getElementsByTagName('input')[0].value;
- 	 arr.push(v);
 	 if (r == v) {
      	set(2);
      	return;
      }
-     console.log(r)
 	 var guessDiff = Math.abs(r - v);
-	 console.log(guessDiff)
-	 guessDiff > 25 ? set(1) : set(0);
+	 if (prdiff && prdiff > guessDiff) {
+		guessDiff > 25 ? set(1) : set(0);
+	 }
+	 guessDiff > 25 ? set(1) : set(0);	
  	 function set(arnum) {
 	 	 s.innerHTML = st[arnum];
 	 	 s.style.background= cl[arnum];
  	 }
- 	 // guess equal to previous guess will return colder
  	 if (pr == v) {
  	 	set(1)
  	 }
- 	 // hotter or colder
-
-     
-
-     pr = v;
-
-     // reset button will remove previous guess
-     // generate a new number
-     // set div#status text to "..." 
-     // div#status background color to #333 Without refreshing the page.
-
+	 pr = v;
+     prdiff = guessDiff;
+	}
+	document.getElementsByTagName('input')[2].onclick = function(event) {
+		s.innerHTML = '...';
+	 	s.style.background= '';
+	 	r = Math.random()*101|0;
+	 	pr = null;
+	 	prdiff = null;
+	 	guessDiff = null;
+	 	document.getElementsByTagName('input')[0].value = "";
 	}
 };
